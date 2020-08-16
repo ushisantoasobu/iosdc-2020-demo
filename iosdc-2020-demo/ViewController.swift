1f1f120da07dda4071ce7c2cf5eb1c166ac9df7b
ushisantoasobu <babblemann.shunsee@gmail.com>
2020-08-16 18:03:12 +0900

    create project

---
+++ iosdc-2020-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosdc-2020-demo
+//
+//  Created by Shunsuke Sato on 2020/08/16.
+//  Copyright © 2020 Shunsuke Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
