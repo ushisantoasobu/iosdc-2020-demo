7f272d4ef5b16fb42d8e7c3c700de5a62f775ca4
ushisantoasobu <babblemann.shunsee@gmail.com>
2020-08-16 22:38:20 +0900

    add unittest

---
+++ iosdc-2020-demoTests/SomePresenterTests.swift
@@ -0,0 +1,75 @@
+//
+//  SomePresenterTests.swift
+//  iosdc-2020-demoTests
+//
+//  Created by Shunsuke Sato on 2020/08/16.
+//  Copyright © 2020 Shunsuke Sato. All rights reserved.
+//
+
+import XCTest
+@testable import iosdc_2020_demo
+
+class iosdc_2020_demoTests: XCTestCase {
+
+    var presenter: SomePresenterImpl!
+
+    var view: MockedSomeView!
+    var repository: MockedSomeRepository!
+
+    override func setUpWithError() throws {
+        view = MockedSomeView()
+        repository = MockedSomeRepository()
+        presenter = SomePresenterImpl(view: view, repository: repository)
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testViewDidLoad() throws {
+        repository.stubbedFetchResult = [
+            SomeEntity(id: 999,
+                       name: "",
+                       type: .fuga,
+                       imageUrl: nil,
+                       createdAt: Date())
+        ]
+        presenter.viewDidLoad()
+        XCTAssertEqual(view.invokedFetchedParameters!.someEntities.count, 1)
+        XCTAssertEqual(view.invokedFetchedParameters!.someEntities.first!.id, 999)
+    }
+}
+
+class MockedSomeView: SomeView {
+    var invokedFetched = false
+    var invokedFetchedCount = 0
+    var invokedFetchedParameters: (someEntities: [SomeEntity], Void)?
+    var invokedFetchedParametersList = [(someEntities: [SomeEntity], Void)]()
+    func fetched(someEntities: [SomeEntity]) {
+        invokedFetched = true
+        invokedFetchedCount += 1
+        invokedFetchedParameters = (someEntities, ())
+        invokedFetchedParametersList.append((someEntities, ()))
+    }
+}
+
+class MockedSomeRepository: SomeRepository {
+    var invokedFetch = false
+    var invokedFetchCount = 0
+    var stubbedFetchResult: [SomeEntity]! = []
+    func fetch() -> [SomeEntity] {
+        invokedFetch = true
+        invokedFetchCount += 1
+        return stubbedFetchResult
+    }
+    var invokedDelete = false
+    var invokedDeleteCount = 0
+    var invokedDeleteParameters: (id: Int, Void)?
+    var invokedDeleteParametersList = [(id: Int, Void)]()
+    func delete(id: Int) {
+        invokedDelete = true
+        invokedDeleteCount += 1
+        invokedDeleteParameters = (id, ())
+        invokedDeleteParametersList.append((id, ()))
+    }
+}
