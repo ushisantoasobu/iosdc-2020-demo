17cff863381be0f11fa7bf25bfa4dcda27252613
ushisantoasobu <babblemann.shunsee@gmail.com>
2020-08-16 18:25:34 +0900

    add SomeEntity

---
+++ iosdc-2020-demo/Domain/Entity/SomeEntity.swift
@@ -0,0 +1,17 @@
+//
+//  SomeEntity.swift
+//  iosdc-2020-demo
+//
+//  Created by Shunsuke Sato on 2020/08/16.
+//  Copyright © 2020 Shunsuke Sato. All rights reserved.
+//
+
+import Foundation
+
+struct SomeEntity {
+    let id: Int
+    let name: String
+    let type: SomeType
+    let imageUrl: String?
+    let createdAt: Date
+}
---
+++ iosdc-2020-demo/Domain/Entity/SomeType.swift
@@ -0,0 +1,14 @@
+//
+//  SomeType.swift
+//  iosdc-2020-demo
+//
+//  Created by Shunsuke Sato on 2020/08/16.
+//  Copyright © 2020 Shunsuke Sato. All rights reserved.
+//
+
+import Foundation
+
+enum SomeType {
+    case hoge
+    case fuga
+}
